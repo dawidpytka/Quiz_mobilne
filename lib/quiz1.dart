849051c9fe28d50dc33bae642f24c80036f04148
dawidpytka <44122759+dawidpytka@users.noreply.github.com>
2019-11-16 09:49:13 +0100

    zmiana koloru tla w zaleznosci od odpowiedzi, wyswietlanie napisu: Dobrze lub Zle

--- lib/quiz1.dart
+++ lib/quiz1.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'dart:io';
 import './database.dart';
 import './Result.dart';
 import 'Stages.dart';
@@ -9,7 +9,9 @@ import 'Stages.dart';
 
 int questionNumber = 0;
 Quiz quiz;
-
+var answerColor = Colors.lightBlueAccent;
+bool wVisible = false;
+bool cVisible = false;
 class Quiz{
   int points = 0;
   List<Question> questionList;
@@ -23,6 +25,8 @@ class Quiz{
 
     void getList()async{
       await getDatabase();
+      questionsInit();
+
 
       questionList = await getQuestions();
     }
@@ -45,11 +49,50 @@ class Quiz1 extends StatefulWidget{
   }
 }
 
-class Quiz1State extends State<Quiz1> {
+class Quiz1State extends State<Quiz1> with SingleTickerProviderStateMixin  {
+  //Uses a Ticker Mixin for Animations
+  Animation _animation;
+  AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+        vsync: this,
+        duration: Duration(
+            milliseconds:
+            250)); //specify the duration for the animation & include `this` for the vsyc
+    _animation = ColorTween(begin: Colors.white, end: Colors.white).animate(
+        _animationController); //use Tween animation here, to animate between the values of 1.0 & 2.5.
+
+    _animation.addListener(() {
+      //here, a listener that rebuilds our widget tree when animation.value chnages
+      setState(() {
+
+      });
+    });
+
+    _animation.addStatusListener((status) {
+      //AnimationStatus gives the current status of our animation, we want to go back to its previous state after completing its animation
+      if (status == AnimationStatus.completed) {
+
+        _animationController
+            .reverse(); //reverse the animation back here if its completed
+        wVisible = false;
+        cVisible = false;
+        sleep(Duration(seconds: 1));
+        nextQuestion();
+
+      }
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      backgroundColor: Colors.tealAccent,
+    return WillPopScope(
+        child:Scaffold(
+      backgroundColor: _animation.value,
         body: new Container(
           margin: EdgeInsets.all(MediaQuery.of(context).size.height*0.01),
           child: new Column(
@@ -59,7 +102,10 @@ class Quiz1State extends State<Quiz1> {
                     style: new TextStyle(
                         fontSize: 25.0
                     )),
-                new Padding(padding: EdgeInsets.all(MediaQuery.of(context).size.height*0.2)),
+                new Padding(padding: EdgeInsets.all(MediaQuery.of(context).size.height*0.08)),
+                new AnimatedOpacity(opacity: wVisible ? 1.0 : 0.0, duration: Duration(milliseconds: 250),child:Text("Zła odpowiedź !", style: new TextStyle(fontSize:30.0))),
+                new AnimatedOpacity(opacity: cVisible ? 1.0 : 0.0, duration: Duration(milliseconds: 250),child:Text("Poprawna odpowiedź !", style: new TextStyle(fontSize:30.0))),
+                new Padding(padding: EdgeInsets.all(MediaQuery.of(context).size.height*0.08)),
                 new Text ("${quiz.questionsStage[questionNumber].question}"),
                 new Padding(padding: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.1)),
                 new Row(
@@ -82,7 +128,12 @@ class Quiz1State extends State<Quiz1> {
                       ]
           )
         )
+    ),
+      onWillPop: () {
+        return Future.value(false); // if true allow back else block it
+      },
     );
+
   }
 
 
@@ -99,29 +150,47 @@ class Quiz1State extends State<Quiz1> {
 
   void checkAnswer(var text)
   {
-    Question currentQuestion = quiz.questionsStage[questionNumber];
-    if(text == currentQuestion.correctAnswer)
+    setState(() {
+      Question currentQuestion = quiz.questionsStage[questionNumber];
+      if(text == currentQuestion.correctAnswer)
       {
+        _animation = ColorTween(begin: Colors.white, end: Colors.greenAccent).animate(
+            _animationController);
+        cVisible = true;
         quiz.points++;
         currentQuestion.done = 1;
         updateQuestion(currentQuestion);
       }
+      else {
+        wVisible = true;
+        _animation = ColorTween(begin: Colors.white, end: Colors.redAccent).animate(
+            _animationController);
+      }
+    });
+
   }
 
-  MaterialButton answerButton(var text)
+  Container answerButton(var text)
   {
-    return new MaterialButton(
-      color: Colors.lightBlueAccent,
-      onPressed: (){checkAnswer(text); nextQuestion();},
-      minWidth: MediaQuery.of(context).size.width * 0.4,
-      height: 100.0,
-      child: new Text(text,
-          style: new TextStyle(
-              color: Colors.black
-          )
+
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.45
+      ,
+      height: MediaQuery.of(context).size.height * 0.13, //multiply the static height value with current animation.value value
+      child: RaisedButton(
+        color: Colors.lightBlueAccent,
+        child: Text(text,
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 15.0),
+          maxLines: 2,
+        ),
+        onPressed: () {
+          checkAnswer(text);
+          _animationController
+              .forward(); // tapping the button, starts the animation.
+        },
       ),
-    );
-  }
+    );}
 }
 
 void questionsInit() async
